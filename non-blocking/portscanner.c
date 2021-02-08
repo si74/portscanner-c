6004a6bc89050099476fb48a7370d0cc2f2603db
Sneha Inguva <snehainguva@Snehas-MBP-2.attlocal.net>
2021-02-07 21:54:57 -0500

    separating non-blocking and blocking scanners

--- non-blocking/portscanner.c
+++ non-blocking/portscanner.c
@@ -1,4 +1,5 @@
-#include <sys/types.h>          
+#include <stdbool.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netdb.h>
@@ -12,48 +13,20 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void client(int port) {
-    int sockfd;
-    int errnum;
-    struct sockaddr_in servaddr;
+// TODO libevent
+// TODO add concurrency, ncat listen, while running portscanner
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-
-    // make socket non-blocking
-    fcntl(sockfd, F_SETFL, O_NONBLOCK);
-
-    // int socket(int domain, int type, int protocol);
-    if(sockfd == -1) {
-	errnum = errno;
-        //printf("Socket init error: %d!!!", errnum);
-        fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
-        goto out;
-    }
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(port);
-
-    //int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-    if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
-	printf("open port not found\n");
-        goto out;
-    } else {
-        printf("open port %d\n", port);
-    }
-
-out:
-    close(sockfd);
+bool FD_IS_ANY_SET(fd_set const *fdset)
+{
+    static fd_set empty;     // initialized to 0 -> empty
+    return memcmp(fdset, &empty, sizeof(fd_set)) != 0;
 }
 
-// TODO libevent
-// TODO add concurrency, ncat listen, while running portscanner 
-
 int main() {
-   
+
     fd_set master;
     fd_set read_fds;
-    int fdmax = 80; // one less than largest fd bc index 0
+    int fdmax = 81; // one less than largest fd bc index 0
 
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
@@ -62,17 +35,17 @@ int main() {
     // for each port try client connection
     // print ports with server-client connection
     int fd_array[1];
-    for(int i = 80; i <= fdmax; i++) {
+    for(int i = 81; i <= fdmax; i++) {
            int sockfd;
            int errnum;
            struct sockaddr_in servaddr;
-       
+
            sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	   printf("i: %d, sockfd: %d\n", i, sockfd); 
-           fd_array[i] = sockfd;
+	   printf("trying to connect i: %d, sockfd: %d\n", i, sockfd);
+           fd_array[0] = sockfd;
            // make socket non-blocking
            fcntl(sockfd, F_SETFL, O_NONBLOCK);
-       
+
            // int socket(int domain, int type, int protocol);
            if(sockfd == -1) {
        	       errnum = errno;
@@ -87,41 +60,60 @@ int main() {
            servaddr.sin_family = AF_INET;
            servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
            servaddr.sin_port = htons(i);
-       
+
            //int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
            connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-       	   
+
     }
 
     int optval;
     int optlen;
     int errnum;
+    int totalcount = 0;
 
     for (;;) {
         // copy master
         read_fds = master;
+        printf("trying to select\n");
+        if (FD_IS_ANY_SET(&master) == false) {
+                break;
+        }
+
         if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
 	    perror("select");
 	    exit(4);
 	}
-        for(int i = 80; i <= fdmax; i++) {
-		//printf("iterating fd = %d\n", fd_array[i]);
-        	if (FD_ISSET(80 , &read_fds)) {
-                        //printf("fd = %d\n", fd_array[i]);
 
-			if (getsockopt(80, SOL_SOCKET, SO_ERROR, &optval, &optlen) < 0) {
-				
+        for(int i = 81; i <= fdmax; i++) {
+		//printf("iterating fd = %d\n", fd_array[i]);
+                printf("trying to check: port %d, fd %d\n", i, fd_array[0]);
+        	if (FD_ISSET(fd_array[0] , &read_fds)) {
+                        //printf("fd = %d\n", fd_array[i]);
+                        printf("isset: port %d, fd %d\n", i, fd_array[0]);
+			if (getsockopt(fd_array[0], SOL_SOCKET, SO_ERROR, &optval, &optlen) < 0) {
+
 			  	// remove i from master
 				//if(errno == )
 				errnum = errno;
 				fprintf(stderr, "getsockopt error: %s\n", strerror( errnum ));
- 				
+
 			} else {
-				printf("fd = %d, optval = %d\n", 80, optval);
+				printf("getsockopt succeeded: fd = %d, optval = %d\n", 81, optval);
+                                if (optval == EINPROGRESS) {
+                                        continue;
+                                }
+                                if (optval == ECONNREFUSED) {
+                                        printf("connection refused. port is closed");
+                                        FD_CLR(fd_array[0],&master);
+                                        close(fd_array[0]);
+                                }
+                                if (optval == 0) {
+                                        printf("port is open!");
+                                }
 			}
+
 		}
         }
-	break;
     }
     return 0;
 }
